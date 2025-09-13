ef93c4bef713cb15d4b990eb1da8f82b5231a20f
Ian Goodacre <IanGoodacre3@gmail.com>
2025-09-14 03:18:09 +1200

    Workaround node's buggy Happy Eyeballs implementation

--- get-cedict.js
+++ get-cedict.js
@@ -6,6 +6,11 @@
 const fs = require('fs');
 const { Readable } = require('stream');
 const zlib = require('node:zlib');
+const net = require('net');
+
+// Workaround node's buggy Happy Eyeballs implementation
+// See: https://github.com/nodejs/node/issues/54359
+net.setDefaultAutoSelectFamilyAttemptTimeout(1000);
 
 const stream = fs.createWriteStream('data/cedict_ts.u8');
 fetch('https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz')
